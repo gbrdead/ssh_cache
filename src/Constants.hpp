e7deb46171e717219d648893354e955b99dc1833
Vladimir Panov <gbr@voidland.org>
2012-12-07 20:41:13 +0000

    Accept socket.
    git-svn-id: file:///tmp/svn/ssh_cache/trunk@680 e3f1fecc-a54b-4937-a0f3-804bd50b2953

---
+++ src/Constants.hpp
@@ -0,0 +1,17 @@
+#ifndef _SSH_CACHE_CONSTANTS_HPP_
+#define _SSH_CACHE_CONSTANTS_HPP_
+
+namespace org
+{
+namespace voidland
+{
+namespace ssh_cache
+{
+
+const unsigned short PORT = 2222;
+
+}
+}
+}
+
+#endif
--- src/SSHCache.cpp
+++ src/SSHCache.cpp
@@ -1,10 +1,33 @@
+#include "Constants.hpp"
+
 #include <boost/asio.hpp>
 using namespace boost::asio;
+using namespace boost::asio::ip;
 
+namespace org
+{
+namespace voidland
+{
+namespace ssh_cache
+{
 
 int main(void)
 {
     io_service ioService;
-
+    tcp::acceptor acceptor(ioService, tcp::endpoint(tcp::v6(), PORT));
+    tcp::socket socket(ioService);
+    acceptor.accept(socket);
     return 0;
+}
+
+
+
+}
+}
+}
+
+
+int main(void)
+{
+    return org::voidland::ssh_cache::main();
 }
