38c26a400e5e3ad6a21386591e80103cf31a55ef
Vladimir Panov <gbr@voidland.org>
2012-12-09 22:13:11 +0000

    Accepting threads
    git-svn-id: file:///tmp/svn/ssh_cache/trunk@681 e3f1fecc-a54b-4937-a0f3-804bd50b2953

--- src/SSHCache.cpp
+++ src/SSHCache.cpp
@@ -1,8 +1,16 @@
 #include "Constants.hpp"
 
 #include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
 using namespace boost::asio;
 using namespace boost::asio::ip;
+using namespace boost::system;
+using namespace boost;
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
 
 namespace org
 {
@@ -11,12 +19,79 @@ namespace voidland
 namespace ssh_cache
 {
 
+static void acceptThread(io_service *ioService, tcp::acceptor *acceptor)
+{
+    while (true)
+    {
+        tcp::socket socket(*ioService);
+        acceptor->accept(socket);
+
+    }
+}
+
 int main(void)
 {
     io_service ioService;
-    tcp::acceptor acceptor(ioService, tcp::endpoint(tcp::v6(), PORT));
-    tcp::socket socket(ioService);
-    acceptor.accept(socket);
+
+    tcp::acceptor *v6Acceptor = NULL;
+    tcp::acceptor *v4Acceptor = NULL;
+    try
+    {
+        v6Acceptor = new tcp::acceptor(ioService, tcp::endpoint(tcp::v6(), PORT));
+    }
+    catch (system_error &e)
+    {
+        cerr << "Cannot create TCP server socket on port " << PORT << ", protocol IPv6: " << e.what() << endl;
+    }
+    try
+    {
+        bool v4AlreadyBound = false;
+        if (v6Acceptor != NULL)
+        {
+            v6_only v6OnlyOption;
+            v6Acceptor->get_option(v6OnlyOption);
+            v4AlreadyBound = !v6OnlyOption.value();
+        }
+        if (!v4AlreadyBound)
+        {
+            v4Acceptor = new tcp::acceptor(ioService, tcp::endpoint(tcp::v4(), PORT));
+        }
+    }
+    catch (system_error &e)
+    {
+        cerr << "Cannot create TCP server socket on port " << PORT << ", protocol IPv4: " << e.what() << endl;
+    }
+
+    if (v4Acceptor != NULL)
+    {
+
+    }
+
+    thread *v4Thread = NULL;
+    thread *v6Thread = NULL;
+    if (v4Acceptor != NULL)
+    {
+        v4Thread = new thread(acceptThread, &ioService, v4Acceptor);
+    }
+    if (v6Acceptor != NULL)
+    {
+        v6Thread = new thread(acceptThread, &ioService, v6Acceptor);
+    }
+
+    if (v4Thread != NULL)
+    {
+        v4Thread->join();
+        delete v4Thread;
+        delete v4Acceptor;
+    }
+
+    if (v6Thread != NULL)
+    {
+        v6Thread->join();
+        delete v6Thread;
+        delete v6Acceptor;
+    }
+
     return 0;
 }
 
