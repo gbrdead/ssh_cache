ba7a1ba5295046a3f64d224ca1e78b84f977849f
Vladimir Panov <gbr@voidland.org>
2013-02-19 16:04:37 +0000

    Streamlining
    git-svn-id: file:///tmp/svn/ssh_cache/trunk@718 e3f1fecc-a54b-4937-a0f3-804bd50b2953

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
-#include "SSHCache.hpp"
+#include "Server.hpp"
+#include "Options.hpp"
+using namespace org::voidland::ssh_cache;
+
+#include <exception>
+#include <iostream>
+using namespace std;
+
 
 int main(int argc, char *argv[])
 {
-    return org::voidland::ssh_cache::main(argc, argv);
+    try
+    {
+        Options options(argc, argv);
+        if (options.isHelp())
+        {
+            cout << options.getDescription();
+            return 0;
+        }
+
+        Server server(options);
+        server.run();
+    }
+    catch (const std::exception &e)
+    {
+        cerr << e.what() << endl;
+        return 1;
+    }
+    return 0;
 }
